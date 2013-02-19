db4dd1713efdc42943d0743b574a03ded0756022
Aaron Scherer <aequasi@gmail.com>
2013-02-18 16:16:33 -0800

    Adding deciderLogic to the WOrkflow code

--- Uecode/Bundle/AmazonBundle/Component/SimpleWorkFlow/Workflow.php
+++ Uecode/Bundle/AmazonBundle/Component/SimpleWorkFlow/Workflow.php
@@ -5,7 +5,12 @@
  */
 namespace Uecode\Bundle\AmazonBundle\Component\SimpleWorkFlow;
 
+// Amazon Components
 use \Uecode\Bundle\AmazonBundle\Component\AmazonComponent;
+
+// Amazon Exceptions
+use \Uecode\Bundle\AmazonBundle\Exception\InvalidConfigurationException;
+use \Uecode\Bundle\AmazonBundle\Exception\InvalidDeciderLogicException;
 
 // Amazon Classes
 use \AmazonSWF;
@@ -18,6 +23,17 @@ class Workflow extends AmazonComponent
 	 */
 	private $workflow;
 
+	/**
+	 * @var \Closure
+	 */
+	private $deciderLogic = null;
+
+	/**
+	 * Builds the Workflow
+	 *
+	 * @param \AmazonSWF $swf
+	 * @param array $workflowType
+	 */
 	public function __construct( AmazonSWF $swf, array $workflowType )
 	{
 
@@ -25,6 +41,12 @@ class Workflow extends AmazonComponent
 
 		$this->workflow = $this->getWorkflow( $workflowType );
 	}
+
+	/********************* Core Logic *********************
+	 *
+	 * Core Logic for our overrode Amazon Class
+	 *
+	 */
 
 	public function run( $taskList = null )
 	{
@@ -36,21 +58,58 @@ class Workflow extends AmazonComponent
 			);
 
 			$response = $this->swf->poll_for_decision_task($opts);
+			if ( $response->isOK( ) ) {
+				$taskToken = (string) $response->body->taskToken;
+
+				if ( !empty( $taskToken ) ) {
+					$deciderResponse = $this->decide( $response );
+
+					$return = array(
+						'task'   => $taskToken,
+						'result' => $deciderResponse
+					);
+					echo json_encode( $return );
+				}
+			}
+			sleep( 2 );
 		}
 	}
 
+	protected function decide( $response )
+	{
+		$function = $this->getDeciderLogic();
+		if( null !== $function || !is_callable( $function ) ) {
+			return $function( $this, $response );
+		}
+
+		throw new InvalidDeciderLogicException();
+	}
+
+	/********************* Getters and Setters *********************
+	 *
+	 * Functions to help initialize
+	 *
+	 */
+
+	/**
+	 * Returns the amazon swf workflow Object
+	 *
+	 * @param array $workflowType
+	 * @return mixed
+	 * @throws InvalidConfigurationException
+	 */
 	public function getWorkflow( array $workflowType )
 	{
 		if( !array_key_exists( 'name', $workflowType ) ) {
-			throw new \Uecode\Bundle\AmazonBundle\Exception\InvalidConfigurationException( "Name must be included in the second argument." );
+			throw new InvalidConfigurationException( "Name must be included in the second argument." );
 		}
 
 		if( !array_key_exists( 'version', $workflowType ) ) {
-			throw new \Uecode\Bundle\AmazonBundle\Exception\InvalidConfigurationException( "Version must be included in the second argument." );
+			throw new InvalidConfigurationException( "Version must be included in the second argument." );
 		}
 
 		if( !array_key_exists( 'defaultTaskList', $workflowType ) ) {
-			throw new \Uecode\Bundle\AmazonBundle\Exception\InvalidConfigurationException( "Version must be included in the second argument." );
+			throw new InvalidConfigurationException( "Version must be included in the second argument." );
 		}
 
 		/** @var $swf AmazonSWF */
@@ -58,6 +117,22 @@ class Workflow extends AmazonComponent
 		$swf->register_workflow_type( $workflowType );
 
 		return $swf->describe_workflow_type( $workflowType );
+	}
+
+	/**
+	 * @param callable $deciderLogic
+	 */
+	public function setDeciderLogic( \Closure $deciderLogic )
+	{
+		$this->deciderLogic = $deciderLogic;
+	}
+
+	/**
+	 * @return callable
+	 */
+	public function getDeciderLogic()
+	{
+		return $this->deciderLogic;
 	}
 
 }
---
+++ Uecode/Bundle/AmazonBundle/Exception/SimpleWorkFlow/InvalidDeciderLogic.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: Aaron Scherer
+ * Date: 2/9/13
+ */
+namespace Uecode\Bundle\AmazonBundle\Exception;
+
+use \Exception;
+
+class InvalidDeciderLogicException extends Exception { }
