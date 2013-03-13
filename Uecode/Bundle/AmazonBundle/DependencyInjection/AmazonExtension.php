a851beb1436dd289dc99311135c5b36572130bc2
Aaron Scherer <aequasi@gmail.com>
2013-03-13 13:33:40 -0700

    Adding new service

--- Uecode/Bundle/AmazonBundle/DependencyInjection/AmazonExtension.php
+++ Uecode/Bundle/AmazonBundle/DependencyInjection/AmazonExtension.php
@@ -21,37 +21,12 @@ class AmazonExtension extends Extension
 	 */
 	public function load( array $configs, ContainerBuilder $container )
 	{
-		$configuration = new Configuration( $container->getParameter( 'kernel.debug' ) );
-		$config = $this->processConfiguration( $configuration, $configs );
-
-		$this->setParameters( $container, $config );
-var_dump( $config );
-		foreach( $config[ 'uecode' ][ 'amazon' ][ 'accounts' ]['connections'] as $name => $account ) {
-			$account ['name' ] = $name;
-			$config = new Config( $account );
-			$factory = new \Uecode\Bundle\AmazonBundle\Factory\AmazonFactory( $config );
-			$container->setParameter( trim( 'uecode.amazon.factory.' . $name ), $factory );
-		}
+		$loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+		$loader->load( 'services.yml' );
 	}
 
-	public function getConfiguration(array $config, ContainerBuilder $container)
+	public function getXsdValidationBasePath()
 	{
-		return new Configuration( $container->getParameter( 'kernel.debug' ) );
-	}
-
-	private function setParameters( ContainerBuilder $container, array $configs, $prefix = 'uecode' )
-	{
-		foreach( $configs as $key => $value )
-		{
-			if( is_array( $value ) )
-			{
-				$this->setParameters( $container, $configs[ $key ], ltrim( $prefix . '.' . $key, '.' ) );
-				$container->setParameter(  ltrim( $prefix . '.' . $key, '.' ), $value );
-			}
-			else
-			{
-				$container->setParameter( ltrim( $prefix . '.' . $key, '.' ), $value );
-			}
-		}
+		return __DIR__ . '/../Resources/config/';
 	}
 }
