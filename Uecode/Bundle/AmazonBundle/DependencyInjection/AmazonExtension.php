11dea96b93d17dae5e9bb954c058c8e443a6fc34
Aaron Scherer <aequasi@gmail.com>
2013-03-13 13:54:56 -0700

    Fixing shit again\.

--- Uecode/Bundle/AmazonBundle/DependencyInjection/AmazonExtension.php
+++ Uecode/Bundle/AmazonBundle/DependencyInjection/AmazonExtension.php
@@ -11,6 +11,8 @@ use \Symfony\Component\Config\FileLocator;
 use \Symfony\Component\DependencyInjection\Loader;
 use \Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
+use Uecode\Bundle\UecodeBundle\Component\Config;
+
 /**
  * Uecode  Extension
  */
@@ -21,8 +23,15 @@ class AmazonExtension extends Extension
 	 */
 	public function load( array $configs, ContainerBuilder $container )
 	{
-		$loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
-		$loader->load( 'services.yml' );
+		//$loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+		//$loader->load( 'services.yml' );
+
+		foreach( $container->getParameter( 'uecode.amazon.accounts.connections' ) as $name => $account ) {
+			$account ['name' ] = $name;
+			$config = new Config( $account );
+			$factory = new \Uecode\Bundle\AmazonBundle\Factory\AmazonFactory( $config );
+			$container->set( trim( 'uecode.amazon.factory.' . $name ), $factory );
+		}	
 	}
 
 	public function getXsdValidationBasePath()
