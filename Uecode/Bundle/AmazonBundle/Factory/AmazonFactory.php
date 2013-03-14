23925eefef1aff68994985a98f4eea3f8c0ad3d8
Aaron Scherer <aequasi@gmail.com>
2013-03-14 11:51:21 -0700

    Fixing Formatting

--- Uecode/Bundle/AmazonBundle/Factory/AmazonFactory.php
+++ Uecode/Bundle/AmazonBundle/Factory/AmazonFactory.php
@@ -24,23 +24,23 @@ use Uecode\Bundle\AmazonBundle\Model\AmazonInterface;
 class AmazonFactory implements FactoryInterface
 {
 
-    /**
-     * @var Config Config for models
-     */
-    private $modelConfig;
+	/**
+	 * @var Config Config for models
+	 */
+	private $modelConfig;
 
-    /**
-     * @var Config Config for account
-     */
-    private $accountConfig;
+	/**
+	 * @var Config Config for account
+	 */
+	private $accountConfig;
 
-    public function __construct( Config $accountConfig )
-    {
-        $this->setAccountConfig( $accountConfig );
+	public function __construct( Config $accountConfig )
+	{
+		$this->setAccountConfig( $accountConfig );
 
-        $file = __DIR__ . '/../Resources/config/models.yml';
-        $this->setModelConfig( new Config( Yaml::parse( $file ) ) );
-    }
+		$file = __DIR__ . '/../Resources/config/models.yml';
+		$this->setModelConfig( new Config( Yaml::parse( $file ) ) );
+	}
 
 
 	/**
@@ -52,8 +52,8 @@ class AmazonFactory implements FactoryInterface
 	 * @return CFRuntime|bool Returns the amazon class
 	 * @throws ClassNotFoundException|InvalidClassException
 	 */
-    public function build( $className, array $options = array() )
-    {
+	public function build( $className, array $options = array() )
+	{
 		$class = $this->checkAmazonClass( $className );
 		if ( $class !== false ) {
 
@@ -79,94 +79,94 @@ class AmazonFactory implements FactoryInterface
 		throw new ClassNotFoundException( $className );
 	}
 
-    /**
-     * Checks to see if the Amazon Class given exists.
-     * Testing if it has Amazon in the name as well
-     *
-     * @param string $class
-     * @param bool $checkAmazon
-     * @return bool|string
-     */
-    public function checkAmazonClass( $class, $checkAmazon = true )
-    {
-	    // Check to see if we have the Amazon class
-        if ( class_exists( $class ) ) {
+	/**
+	 * Checks to see if the Amazon Class given exists.
+	 * Testing if it has Amazon in the name as well
+	 *
+	 * @param string $class
+	 * @param bool $checkAmazon
+	 * @return bool|string
+	 */
+	public function checkAmazonClass( $class, $checkAmazon = true )
+	{
+		// Check to see if we have the Amazon class
+		if ( class_exists( $class ) ) {
 
-	        // If we do, check to see if we have a Uecode version of this class and replace $class with that
-            $this->checkUecodeClass( $class );
+			// If we do, check to see if we have a Uecode version of this class and replace $class with that
+			$this->checkUecodeClass( $class );
 
-            return $class;
-        }
+			return $class;
+		}
 
-        // If $class doesnt exist, check for Amazon$class
-        if ( $checkAmazon ) {
-            return $this->checkAmazonClass( 'Amazon' . $class, false );
-        }
+		// If $class doesnt exist, check for Amazon$class
+		if ( $checkAmazon ) {
+			return $this->checkAmazonClass( 'Amazon' . $class, false );
+		}
 
-        return false;
-    }
+		return false;
+	}
 
-    /**
-     * Checks to see if there is a Uecode extension of the given class
-     * If there is renames $className
-     *
-     * @param string $className
-     * @throws InvalidClassException
-     */
-    public function checkUecodeClass( &$className )
-    {
+	/**
+	 * Checks to see if there is a Uecode extension of the given class
+	 * If there is renames $className
+	 *
+	 * @param string $className
+	 * @throws InvalidClassException
+	 */
+	public function checkUecodeClass( &$className )
+	{
 
-	    // Run through the configs
-        foreach ( $this->getModelConfig()->all() as $model ) {
+		// Run through the configs
+		foreach ( $this->getModelConfig()->all() as $model ) {
 
-	        // If we have an override for the amazon class, replace it
-            if ( $className === $model[ 'amazon_class' ] ) {
-                $className = $model[ 'uecode_class' ];
-            }
-        }
+			// If we have an override for the amazon class, replace it
+			if ( $className === $model[ 'amazon_class' ] ) {
+				$className = $model[ 'uecode_class' ];
+			}
+		}
 
-	    // If the given class doesn't exist, throw an error.
-	    // This shouldn't happen
-        if ( !class_exists( $className ) ) {
-            throw new InvalidClassException( $className );
-        }
-    }
+		// If the given class doesn't exist, throw an error.
+		// This shouldn't happen
+		if ( !class_exists( $className ) ) {
+			throw new InvalidClassException( $className );
+		}
+	}
 
-    /**
-     * @param Config $accountConfig
-     * @return AmazonFactory
-     */
-    public function setAccountConfig( Config $accountConfig )
-    {
-        $this->accountConfig = $accountConfig;
+	/**
+	 * @param Config $accountConfig
+	 * @return AmazonFactory
+	 */
+	public function setAccountConfig( Config $accountConfig )
+	{
+		$this->accountConfig = $accountConfig;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return \Uecode\Bundle\UecodeBundle\Component\Config
-     */
-    public function getAccountConfig()
-    {
-        return $this->accountConfig;
-    }
+	/**
+	 * @return \Uecode\Bundle\UecodeBundle\Component\Config
+	 */
+	public function getAccountConfig()
+	{
+		return $this->accountConfig;
+	}
 
-    /**
-     * @param Config $modelConfig
-     * @return AmazonFactory
-     */
-    public function setModelConfig( Config $modelConfig )
-    {
-        $this->modelConfig = $modelConfig;
+	/**
+	 * @param Config $modelConfig
+	 * @return AmazonFactory
+	 */
+	public function setModelConfig( Config $modelConfig )
+	{
+		$this->modelConfig = $modelConfig;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    /**
-     * @return \Uecode\Bundle\UecodeBundle\Component\Config
-     */
-    public function getModelConfig()
-    {
-        return $this->modelConfig;
-    }
+	/**
+	 * @return \Uecode\Bundle\UecodeBundle\Component\Config
+	 */
+	public function getModelConfig()
+	{
+		return $this->modelConfig;
+	}
 }
