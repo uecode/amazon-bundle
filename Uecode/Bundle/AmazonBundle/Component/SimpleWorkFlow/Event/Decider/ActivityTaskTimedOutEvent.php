9e7b50b589844b7022a8c21840eff44698ecb27b
John Pancoast <jpancoast@undergroundelephant.com>
2013-03-19 14:28:53 -0700

    Renamed decider event namespace

--- Uecode/Bundle/AmazonBundle/Component/SimpleWorkFlow/Event/Decider/ActivityTaskTimedOutEvent.php
+++ Uecode/Bundle/AmazonBundle/Component/SimpleWorkFlow/Event/Decider/ActivityTaskTimedOutEvent.php
@@ -3,23 +3,22 @@
  * @author Aaron Scherer
  * @date   2/20/13
  */
-namespace Uecode\Bundle\AmazonBundle\Component\SimpleWorkFlow\Event\Type\Decider;
+namespace Uecode\Bundle\AmazonBundle\Component\SimpleWorkFlow\Event\Decider;
 
 // Amazon Bundle's SWF Components
 use \Uecode\Bundle\AmazonBundle\Component\SimpleWorkFlow\Event\AbstractEvent;
 use \Uecode\Bundle\AmazonBundle\Component\SimpleWorkFlow\State\DeciderWorkerState;
 
-class ActivityTaskFailedEvent extends AbstractEvent
+class ActivityTaskTimedOutEvent extends AbstractEvent
 {
 
 	public function __construct()
 	{
-		$this->setEventType( 'ActivityTaskFailed' );
-
+		$this->setEventType( 'ActivityTaskTimedOut' );
 		$this->setEventLogic( function( $event, &$workflowState, &$timerOptions, &$activityOptions, &$continueAsNew, &$maxEventId ) {
 			// @TODO
 			// Need logic
-			// when an activity fails, a real application may want to retry it or report the incident
+			// when an activity times out, a real application may want to retry it or report the incident
 		} );
 	}
 }
